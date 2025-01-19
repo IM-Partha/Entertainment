85b8abe2192fc6099e7a50ec554c9a4123177310
Partha Debnath <85936227+IM-Partha@users.noreply.github.com>
2025-01-19 09:06:51 +0530

    first commit

---
+++ backend/src/models/user.models.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+
+// database schema of user 
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+})
+
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User }
---
+++ frontend/src/components/CssComponents/NavbarMenu.jsx
@@ -0,0 +1,24 @@
+
+import React, { useState } from 'react';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import AppsIcon from '@mui/icons-material/Apps';
+import { Button, Box } from '@mui/material';
+
+
+
+export default function NavbarMenu() {
+    const ITEM_HEIGHT = 48;
+  
+
+
+    
+
+   
+
+    return (
+        <div></div>
+    );
+}
+
